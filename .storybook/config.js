9a760f96e1a05be84aadaadbbeca2511fdefe83c
mariadoesmath <miaeskro@gmail.com>
2019-07-05 13:58:33 -0500

    Update storybook config

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,6 @@
 import { configure } from '@storybook/react';
 
-var req = require.context('../components', true, /\.stories\.jsx$/);
+var req = require.context('../src/components', true, /\.stories\.jsx$/);
 
 configure(() => {
   req.keys().forEach((filename) => req(filename));
